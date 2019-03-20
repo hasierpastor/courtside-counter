aee6fd94fad3d7b9d87cdcf46b1386061acab03a
Hasier Pastor <hasier.pastor@gmail.com>
2019-03-20 12:32:06 -0700

    fixed mongo setup bugs and add route for players

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const playerRoutes = require('./routes/players');
+const MongoClient = require('mongodb').MongoClient;
 
 app.use(express.json());
 
 app.use(cors());
 
-//put in deck routes middleware
-app.use('/players', playerRoutes);
+// set up a connection to the server running on localhost (mongod)
+const mongo = new MongoClient('mongodb://localhost:27017', {
+  useNewUrlParser: true
+});
+
+let db;
+
+/**
+ * Route handler for GET to /pets
+ */
+app.get('/players', function(req, res, next) {
+  db.collection('players') // query players
+    .find() // find all, view result as an array
+    .toArray(function(error, result) {
+      // a standard Node.js "error-first" callback
+      if (error) {
+        return res.json({ error });
+      }
+      // return the JSON of the result array
+      return res.json(result);
+    });
+});
 
 /** 404 handler */
 
@@ -28,6 +48,28 @@ app.use(function(err, req, res, next) {
   return res.json({
     error: err,
     message: err.message
+  });
+});
+
+/**
+ * Database connection and server set-up
+ */
+mongo.connect(function(error) {
+  // a standard Node.js "error-first" callback
+  if (error) {
+    // kill express if we cannot connect to the database server
+    console.error(error);
+    process.exit(1);
+  }
+
+  // if we reach this line, we've made it
+  console.log('Successfully connected to database');
+  // set the active database
+  db = mongo.db('basketball_db');
+
+  // standard express app.listen
+  app.listen(3333, function() {
+    console.log('Courtside Counter API Server listening on port 3333.');
   });
 });
 
