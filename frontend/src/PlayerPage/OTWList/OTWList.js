4b6f59387afc8f25916fe765a7bef56021eb4f2d
Silas Burger <ybsilas@gmail.com>
2019-03-29 13:25:47 -0700

    fixed some silly errors

---
+++ frontend/src/PlayerPage/OTWList/OTWList.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import CourtsideCounterApi from '../../util/CourtsideCounterAPI';
+// import { getPlayerData } from '../../util/mockAPI';
+
+class OTWList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      OTWPlayers: [],
+    };
+  }
+
+  async componentDidMount() {
+    try {
+      const OTWPlayers = await CourtsideCounterApi.getOTW(
+        this.props.currUser._token 
+      );
+      this.setState({ OTWPlayers});
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  render() {
+    const OTWPlayers = this.state.OTWPlayers.map(player => (
+      <div key={player._id}>{player.name}</div>
+    ));
+    return <div>{OTWPlayers}</div>;
+  }
+}
+
+export default OTWList;
